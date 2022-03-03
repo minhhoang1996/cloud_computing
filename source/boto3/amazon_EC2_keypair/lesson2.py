541a0784935465434bd7bf04429eea0bb29b7200
minhhoang1996 <hoangson31096@gmail.com>
2022-03-03 15:09:23 +0700

    ec2: Amazon EC2 Keypair
    Signed-off-by: minhhoang1996 <hoangson31096@gmail.com>

---
+++ source/boto3/amazon_EC2_keypair/lesson2.py
@@ -0,0 +1,32 @@
+import boto3
+from source.boto3.manage_EC2_instance.lesson1 import DemoEc2
+
+
+class ExampleKeypair(DemoEc2):
+    def __init__(self, access_key_id, secret_access_key, region):
+        DemoEc2.__init__(self, access_key_id, secret_access_key, region)
+
+    def get_keypair_info(self):
+        print(self.ec2.describe_key_pairs())
+
+    def create_keypair(self, key_name="keypair_default"):
+        response = self.ec2.create_key_pair(KeyName=key_name)
+        print(response)
+
+    def delete_keypair(self, key_name="keypair_default"):
+        response = self.ec2.delete_key_pair(KeyName=key_name)
+        print(response)
+
+
+if __name__ == "__main__":
+    aws_access_key_id = ""
+    aws_secret_access_key = ""
+    region_name = "us-east-1"
+    keypair = ExampleKeypair(aws_access_key_id,
+                             aws_secret_access_key,
+                             region_name)
+
+    keypair.get_keypair_info()
+    keypair.create_keypair(key_name="demo_keypair")
+    keypair.delete_keypair(key_name="demo_keypair")
+    keypair.get_keypair_info()
